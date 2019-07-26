c9f5538eb7fbaf566fc922209221461544a60d78
adufi <sneelshneider@gmail.com>
2019-07-26 12:12:36 -0400

    .

--- Resume/settings.py
+++ Resume/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,8 +29,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['a-resume.herokuapp.com']
 
-CSRF_COOKIE_SECURE = False
-CSRF_COOKIE_DOMAIN = '127.0.0.1'
+#  CSRF_COOKIE_SECURE = False
+#  CSRF_COOKIE_DOMAIN = '127.0.0.1'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Application definition
 
@@ -80,15 +85,18 @@ WSGI_APPLICATION = 'Resume.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'Resume_Django',
-        'USER': 'postgre',
-        'PASSWORD': 'toor',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'Resume_Django',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'toor',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
 }
+
+db_from_env = dj_database_url.config(conn_max_age = 600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
