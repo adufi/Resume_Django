80a9949a5768d03b350bb0f6c42c740a2ad7d5e3
adufi <sneelshneider@gmail.com>
2019-07-26 17:21:58 -0400

    Update settings.py

--- Resume/settings.py
+++ Resume/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'jk#fyx0(=bif%vob38kjcapohga3ard+(ad(k_6$1@hju1l=j='
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,11 +87,6 @@ WSGI_APPLICATION = 'Resume.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'Resume_Django',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'toor',
-    #     'HOST': '127.0.0.1',
-    #     'PORT': '5432',
     }
 }
 
